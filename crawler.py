c2a7df797ea9cc3998118013b180b83c5aa9ad4c
Heng Boon Long <pngwenlong@gmail.com>
2023-07-12 16:54:39 +0800

    improve scraping accuracy

--- crawler.py
+++ crawler.py
@@ -19,11 +19,17 @@ def crawl(module):
         for top_level_comment in submission.comments:
             if isinstance(top_level_comment, MoreComments):
                 continue
-            # do not scrape any comments with links (likely to be a generated message)
-            elif re.search('(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)', top_level_comment.body):
+            # do not scrape any comments with links (likely to be a generated message) or comments that were deleted/removed
+            elif re.search('(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)', top_level_comment.body) or\
+                top_level_comment.body == "[deleted]" or top_level_comment.body == "[removed]":
                 continue
-            # ignore deleted comments
-            elif top_level_comment.body == "[deleted]":
+            # ignore posts with other module codes inside
+            elif re.search('[A-Za-z]{2,4}[0-9]{4,4}[A-Za-z]{0,1}', top_level_comment.body) and\
+                (module.lower() not in re.findall('[A-Za-z]{2,4}[0-9]{4,4}[A-Za-z]{0,1}', top_level_comment.body) and\
+                    module.upper() not in re.findall('[A-Za-z]{2,4}[0-9]{4,4}[A-Za-z]{0,1}', top_level_comment.body)):
+                continue
+            # ignore short comments - high chance not related
+            elif len(top_level_comment.body.split(" ")) <= 10:
                 continue
             result.append(top_level_comment.body)
 
